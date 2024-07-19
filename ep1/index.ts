5bad3f7b611e474b1eedb2af6b69471a9c691ff0
ballMguy <supaluk29188@gmail.com>
2024-07-19 11:35:03 +0700

    โปรเจค

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,1 +1,29 @@
 const fullName : string = 'Supaluk'
+
+//การตั้งชื่อตัวแปรแบบ camel case
+const lastName : string = 'Yodjan'
+const nickName : string = 'Ball'
+const robloxUsername : string = 'ballMguy'
+
+//วิธีการตั้งตัวแปร
+var a : number = 69
+let b : string = 'skibidi'
+const c : boolean = true
+
+//เครื่องหมายเปรียบเทียบ
+/*
+    === เท่ากับ
+    >= มากกว่าเท่ากับ
+    <= น้อยกว่าเท่ากับ
+    > มากกว่า
+    < น้อยกว่า
+    !== ไม่เท่ากับ
+*/
+const a : number = 10
+const b : number = 15
+console.log(a === b) //เปรียบเทียบ 
+console.log(a > b) //a < b ไหม
+console.log(a <= b) // a <= b ไหม
+
+
+
