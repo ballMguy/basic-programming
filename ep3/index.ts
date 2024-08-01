3e7ed8999cff0160f1cff7ab1af9b111ce5a9782
ballMguy <supaluk29188@gmail.com>
2024-08-01 18:53:59 +0700

    BMI change

--- ep3/index.ts
+++ ep3/index.ts
@@ -8,19 +8,4 @@ function ทหาร(sex: string, height: number ,weight: number){
 
 ทหาร('male', 170, 49)
 
-function BMIs(height : number, weight: number) {
-    let BMI: number = weight / (height * height)
-    if(BMI > 30) {
-        console.log('อ้วนมาก / โรคอ้วนระดับ 3')
-    } else if(BMI >= 25 && BMI <= 29.90) {
-        console.log('อ้วนมาก / โรคอ้วนระดับ 2')
-    } else if(BMI >= 23 && BMI <= 24.90) {
-        console.log('อ้วนมาก / โรคอ้วนระดับ 1')
-    } else if(BMI >= 18.50 && BMI <= 22.90) {
-        console.log('ปกติ สุขภาพดี') 
-    } else {
-        console.log('น้ำหนักน้อย / ผอม')
-    }
-        
-}
-BMIs(170,45)
+
