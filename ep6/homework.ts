6a87c405ce84dac2972810b6ce1c5d98afcfdf2b
ballMguy <supaluk29188@gmail.com>
2024-09-05 18:43:45 +0700

    homework

---
+++ ep6/homework.ts
@@ -0,0 +1,13 @@
+const personalInfo = {
+    firstName: "ตรีมิตร",
+    lastName: "บุญตะคำ",
+    houseNumber: "7",
+    villageNumber: "16",
+    road: "บ้านคำ",
+    subDistrict: "ศรีตะคำ",
+    district: "กรุงเทพมหานคร",
+    postalCode: "10140",
+    landLine: "02-45678913",
+    telePhone: "089-558-0123",
+    eMail: "trimid.boon@gmail.com"
+}
